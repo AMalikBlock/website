00a22fd73f5aff41217b5b25335dcdccb700c978
Abdullah Malik <amalik.tech@outlook.com>
2025-04-28 12:04:37 -0500

    Fix chrome toggle dak and light mode

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const toggle = document.getElementById('toggleMode');
+
 toggle.addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
+  
   if (document.body.classList.contains('dark-mode')) {
+    toggle.textContent = '🌙';
+    localStorage.setItem('theme', 'dark');
+  } else {
+    toggle.textContent = '🌞';
+    localStorage.setItem('theme', 'light');
+  }
+});
+
+// Keep user choice after refresh
+window.addEventListener('load', () => {
+  const theme = localStorage.getItem('theme');
+  if (theme === 'dark') {
+    document.body.classList.add('dark-mode');
     toggle.textContent = '🌙';
   } else {
     toggle.textContent = '🌞';
